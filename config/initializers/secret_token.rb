2057e991a9080ee9428795a58c2a164af0cbf003
Robert <fgoodiu@gmail.com>
2013-06-13 22:22:49 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brewup::Application.config.secret_token = '0044e93da69c200c9a495e5c578bfd896edf65db9aff9087be77d49ca3a804a409b0e09ace433c75150aeb546911aa00dc7720a084e77760a02d4faf876fc9e1'
