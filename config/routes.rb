b2980ff3fbad759998ad3088e395f20fe231f6f7
Robert <fgoodiu@gmail.com>
2013-06-13 23:46:10 +0200

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Brewup::Application.routes.draw do
+  #root :to => 'welcome#index'
   get "static_pages/home"
 
   get "static_pages/help"
@@ -54,7 +55,7 @@ Brewup::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+
 
   # See how all your routes lay out with "rake routes"
 
