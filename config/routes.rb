d67a867ebfad4678a488cb7f5fe4fa76ef170836
Robert <fgoodiu@gmail.com>
2013-06-14 13:12:22 +0200

    Finish layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Brewup::Application.routes.draw do
-  #root :to => 'welcome#index'
-  get "static_pages/home"
+  
+  
+  get "users/new"
 
-  get "static_pages/help"
+  root :to => 'static_pages#home'
 
-  get "static_pages/about"
+  match '/signup',  to: 'users#new'
+
+
+  match '/help',    to: 'static_pages#help'
+  match '/about',   to: 'static_pages#about'
+  match '/contact', to: 'static_pages#contact'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
@@ -55,7 +61,6 @@ Brewup::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-
 
   # See how all your routes lay out with "rake routes"
 
